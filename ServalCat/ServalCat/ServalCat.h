a299c62e284c1f0f90b1e20a2b5d25039e873f1c
史 翔新 <shi_xiangxin@mages.co.jp>
2017-03-11 14:25:33 +0900

    Initial Xcode project

---
+++ ServalCat/ServalCat/ServalCat.h
@@ -0,0 +1,19 @@
+//
+//  ServalCat.h
+//  ServalCat
+//
+//  Created by 史　翔新 on 2017/03/11.
+//  Copyright © 2017年 Crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ServalCat.
+FOUNDATION_EXPORT double ServalCatVersionNumber;
+
+//! Project version string for ServalCat.
+FOUNDATION_EXPORT const unsigned char ServalCatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServalCat/PublicHeader.h>
+
+
