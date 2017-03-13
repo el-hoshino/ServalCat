82822b3e2f93248af11bf8748b14afbe9d9c2e0e
史 翔新 <shi_xiangxin@mages.co.jp>
2017-03-13 18:43:23 +0900

    Animation duration minor fix

--- ServalCat/ServalCat/ImagePreviewView.swift
+++ ServalCat/ServalCat/ImagePreviewView.swift
@@ -258,7 +258,7 @@ extension ImagePreviewView {
 	
 	private func animate(_ animations: @escaping () -> Void, preSwitching: (() -> Void)?) {
 		
-		UIView.animate(withDuration: 0.2, animations: { 
+		UIView.animate(withDuration: 0.15, animations: {
 			animations()
 			
 		}) { (_) in
